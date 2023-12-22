07dbfd9331f9c6680b5e72004ba3d57f35168d86
Joydip Kanjilal <joydipkanjilal@yahoo.com>
2023-12-22 11:40:20 +0530

    Initial commit

---
+++ PostSharp.Exception.Demo/ExceptionAspect.cs
@@ -0,0 +1,18 @@
+﻿using PostSharp.Aspects;
+
+namespace PostSharp.Exception.Demo
+{
+    [Serializable]
+
+    public class ExceptionAspect : OnExceptionAspect
+    {
+        public override void OnException(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Error occured at: " +
+            DateTime.Now.ToShortTimeString() + " Error Message: " +
+            args.Exception.Message);
+            args.FlowBehavior = FlowBehavior.Continue;
+            base.OnException(args);
+        }
+    }
+}
---
+++ PostSharp.Exception.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using PostSharp.Aspects;
+
+
+PostSharpHelper.TestExceptionAspect();
+
+public static class PostSharpHelper
+{
+    [ExceptionAspect]
+    public static void TestExceptionAspect()
+    {
+        throw new Exception("This is a test message");
+    }
+}
+
+[Serializable]
+public class ExceptionAspect : OnExceptionAspect
+{
+    public override void OnException(MethodExecutionArgs args)
+    {
+        Console.WriteLine("Error occured at: " +
+        DateTime.Now.ToShortTimeString() + " Error Message: " +
+        args.Exception.Message);
+        args.FlowBehavior = FlowBehavior.Continue;
+        base.OnException(args);
+    }
+}
+
