475a60c06a5d8f28594b1394d663a1825dc003aa
Joydip Kanjilal <joydipkanjilal@yahoo.com>
2024-02-02 00:17:14 +0530

    Dispose added...

--- MethodDurationInterceptor.cs
+++ MethodDurationInterceptor.cs
@@ -17,24 +17,28 @@ namespace Autofac.Challenge.MethodDuration.Demo
 
         public void Intercept(IInvocation invocation)
         {
-            var declaringType = invocation.Method.DeclaringType;
-            var methodName = invocation.Method.Name;
+            if (writer != null)
+            {
+                var declaringType = invocation.Method.DeclaringType;
+                var methodName = invocation.Method.Name;
 
-            //Before method execution
-            var stopwatch = Stopwatch.StartNew();
+                //Before method execution
+                var stopwatch = Stopwatch.StartNew();
 
-            //Executing the actual method
-            invocation.Proceed();
+                //Executing the actual method
+                invocation.Proceed();
 
-            //After method execution
-            stopwatch.Stop();
-            writer.WriteLine(
-                "MeasureDurationInterceptor: {0} executed in {1} milliseconds.",
-                invocation.MethodInvocationTarget.Name,
-                stopwatch.Elapsed.TotalMilliseconds.ToString("0.000")
-                );
+                //After method execution
+                stopwatch.Stop();
+                writer.WriteLine(
+                    "The method {0} was executed in {1} milliseconds.",
+                    invocation.MethodInvocationTarget.Name,
+                    stopwatch.Elapsed.TotalMilliseconds.ToString("0.000")
+                    );
 
-            writer.Flush();
+                writer.Flush();
+                writer.Dispose();
+            }
         }
     }
 }
