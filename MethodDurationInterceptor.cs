69367bc4c0d32d75e80290ac0c9bea093c331ecb
Joydip Kanjilal <joydipkanjilal@yahoo.com>
2024-02-01 20:49:56 +0530

    Interceptor updated...

--- MethodDurationInterceptor.cs
+++ MethodDurationInterceptor.cs
@@ -35,36 +35,4 @@ namespace Autofac.Challenge.MethodDuration.Demo
                 );
         }
     }
-    //public class DataInterceptor : IInterceptor
-    //{
-    //    TextWriter writer;
-    //    public DataInterceptor(TextWriter writer)
-    //    {
-    //        if (writer == null)
-    //        {
-    //            throw new ArgumentNullException(nameof(writer));
-    //        }
-    //        this.writer = writer;
-    //    }
-    //    public void Intercept(IInvocation invocation)
-    //    {
-    //        var declaringType = invocation.Method.DeclaringType;
-    //        var methodName = invocation.Method.Name;
-
-    //        var name = $"{declaringType}.{methodName}";
-    //        var args = string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()));
-
-    //        writer.WriteLine($"Method Name: {name}");
-    //        writer.WriteLine($"Arguments: {args}");
-
-    //        var watch = System.Diagnostics.Stopwatch.StartNew();
-    //        invocation.Proceed();
-    //        watch.Stop();
-    //        var timeTaken = watch.ElapsedMilliseconds;
-
-    //        writer.WriteLine($"Result: {invocation.ReturnValue}");
-    //        writer.WriteLine($"Time Taken: {timeTaken} ms.");
-    //        writer.WriteLine();
-    //    }
-    //}
 }
