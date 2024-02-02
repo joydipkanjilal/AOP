8b496fd35b7eba7b6a24fcaaabde78a935018070
Joydip Kanjilal <joydipkanjilal@yahoo.com>
2024-02-02 13:33:49 +0530

    Fixed......

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ using System.Threading.RateLimiting;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+builder.Services.AddRateLimiter(_ => _
+    .AddFixedWindowLimiter(policyName: "fixed", options =>
+    {
+        options.PermitLimit = 1;
+        options.Window = TimeSpan.FromSeconds(15);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 1;
+
+    }));
+
 //var concurrencyPolicy = "Concurrency";
 //builder.Services.AddRateLimiter(rateLimiterOptions =>
 //{
@@ -20,21 +30,21 @@ builder.Services.AddControllers();
 //    });
 //});
 
-builder.Services.AddRateLimiter(options => {
-    options.RejectionStatusCode = 429;
-    options.AddTokenBucketLimiter(policyName: "token", options => {
-        options.TokenLimit = 1;
-        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-        options.QueueLimit = 1;
-        options.ReplenishmentPeriod = TimeSpan.FromSeconds(10);
-        options.TokensPerPeriod = 1;
-        options.AutoReplenishment = true;
-    });
-});
+//builder.Services.AddRateLimiter(options => {
+//    options.RejectionStatusCode = 429;
+//    options.AddTokenBucketLimiter(policyName: "token", options => {
+//        options.TokenLimit = 1;
+//        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+//        options.QueueLimit = 1;
+//        options.ReplenishmentPeriod = TimeSpan.FromSeconds(10);
+//        options.TokensPerPeriod = 1;
+//        options.AutoReplenishment = true;
+//    });
+//});
 
 var app = ContainerHelper.BuildContainer(builder);
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 
 //if(!Constants.Flag)
@@ -43,7 +53,7 @@ app.MapGet("/", () => "Hello World!");
     {
         //Task.Delay(100, token);
         return dataRepository.GetEmployees();
-    }).RequireRateLimiting("token");
+    }).RequireRateLimiting("fixed");
 
 //Constants.Flag = true;
 //}
